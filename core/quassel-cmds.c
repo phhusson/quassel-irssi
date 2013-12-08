d07dd673d579f650307d4b20d6b83c189f7295cd
Pierre-Hugues Husson <phh@phh.me>
2013-12-08 14:54:41 +0100

    Add direct access to all quassel supported commands

--- core/quassel-cmds.c
+++ core/quassel-cmds.c
@@ -62,12 +62,91 @@ void cmd_qbacklog(const char *arg, Quassel_SERVER_REC *server, WI_ITEM_REC* wi) 
 
 	signal_stop();
 }
+
+static void cmd_self(const char *arg, Quassel_SERVER_REC* server, WI_ITEM_REC* wi) {
+	char *cmd = NULL;
+	int len = asprintf(&cmd, "/%s %s", current_command, arg);
+	(void)len;
+	quassel_irssi_send_message(SERVER(server), wi->visible_name, cmd, 0);
+	free(cmd);
+
+	signal_stop();
+}
+
 void quassel_cmds_init(void) {
 	command_bind_proto("quote", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_quote);
 	command_bind_proto("qbacklog", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_qbacklog);
+
+	//Commands that are sent as is to quassel core
+	command_bind_proto_first("away", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("ban", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("unban", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("ctcp", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("delkey", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("deop", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("dehalfop", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("devoice", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("invite", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("join", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("keyx", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("kick", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("kill", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("list", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("me", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("mode", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("nick", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("notice", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("oper", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("op", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("halfop", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("part", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("ping", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("query", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("setkey", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("showkey", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("topic", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("voice", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("wait", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("who", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
+	command_bind_proto_first("whois", Quassel_PROTOCOL, NULL, (SIGNAL_FUNC) cmd_self);
 }
 
 void quassel_cmds_deinit(void) {
 	command_unbind("quote", (SIGNAL_FUNC) cmd_quote);
 	command_unbind("qbacklog", (SIGNAL_FUNC) cmd_qbacklog);
+
+	//Commands that are sent as is to quassel core
+	//Created from init command with
+	// s/\vcommand_bind_proto_first(.*), Quassel_PROTOCOL, NULL(.*)$/command_unbind\1\2/g
+	command_unbind("away", (SIGNAL_FUNC) cmd_self);
+	command_unbind("ban", (SIGNAL_FUNC) cmd_self);
+	command_unbind("unban", (SIGNAL_FUNC) cmd_self);
+	command_unbind("ctcp", (SIGNAL_FUNC) cmd_self);
+	command_unbind("delkey", (SIGNAL_FUNC) cmd_self);
+	command_unbind("deop", (SIGNAL_FUNC) cmd_self);
+	command_unbind("dehalfop", (SIGNAL_FUNC) cmd_self);
+	command_unbind("devoice", (SIGNAL_FUNC) cmd_self);
+	command_unbind("invite", (SIGNAL_FUNC) cmd_self);
+	command_unbind("join", (SIGNAL_FUNC) cmd_self);
+	command_unbind("keyx", (SIGNAL_FUNC) cmd_self);
+	command_unbind("kick", (SIGNAL_FUNC) cmd_self);
+	command_unbind("kill", (SIGNAL_FUNC) cmd_self);
+	command_unbind("list", (SIGNAL_FUNC) cmd_self);
+	command_unbind("me", (SIGNAL_FUNC) cmd_self);
+	command_unbind("mode", (SIGNAL_FUNC) cmd_self);
+	command_unbind("nick", (SIGNAL_FUNC) cmd_self);
+	command_unbind("notice", (SIGNAL_FUNC) cmd_self);
+	command_unbind("oper", (SIGNAL_FUNC) cmd_self);
+	command_unbind("op", (SIGNAL_FUNC) cmd_self);
+	command_unbind("halfop", (SIGNAL_FUNC) cmd_self);
+	command_unbind("part", (SIGNAL_FUNC) cmd_self);
+	command_unbind("ping", (SIGNAL_FUNC) cmd_self);
+	command_unbind("query", (SIGNAL_FUNC) cmd_self);
+	command_unbind("setkey", (SIGNAL_FUNC) cmd_self);
+	command_unbind("showkey", (SIGNAL_FUNC) cmd_self);
+	command_unbind("topic", (SIGNAL_FUNC) cmd_self);
+	command_unbind("voice", (SIGNAL_FUNC) cmd_self);
+	command_unbind("wait", (SIGNAL_FUNC) cmd_self);
+	command_unbind("who", (SIGNAL_FUNC) cmd_self);
+	command_unbind("whois", (SIGNAL_FUNC) cmd_self);
 }
